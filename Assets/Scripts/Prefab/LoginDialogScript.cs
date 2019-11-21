94290eeced2e8c0f4af26b99880677621e6c83d8
yuki1432 <42318210+yuki1432@users.noreply.github.com>
2019-11-22 00:22:26 +0800

    origin

--- Assets/Scripts/Prefab/LoginDialogScript.cs
+++ Assets/Scripts/Prefab/LoginDialogScript.cs
@@ -45,21 +45,26 @@ namespace Prefab
             _cancel.onClick.AddListener(cancelBtnCallback);
         }
 
-        private void ConfirmBtnCallback()
+        public void RegCallback(Action<bool> callback)
         {
-            string username = _name.text;
-            string userpwd = _pwd.text;
-
-            //发送请求到服务器，验证用户名密码等是否正确
-
-            //如果正确，连接到数据大厅并进入开始界面
-
-            //如果不正确，提示相关的错误信息
+            
         }
-        
-        private void CancelBtnCallback()
-        {
-            //弹出对话框，询问是否退出游戏
-        }
+
+//        private void ConfirmBtnCallback()
+//        {
+//            string username = _name.text;
+//            string userpwd = _pwd.text;
+//
+//            //发送请求到服务器，验证用户名密码等是否正确
+//
+//            //如果正确，连接到数据大厅并进入开始界面
+//
+//            //如果不正确，提示相关的错误信息
+//        }
+//        
+//        private void CancelBtnCallback()
+//        {
+//            //弹出对话框，询问是否退出游戏
+//        }
     }
 }
