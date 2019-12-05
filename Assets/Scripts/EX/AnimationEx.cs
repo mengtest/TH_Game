b3d3e5b74f4f01418dc3c862b12cb2522b419c54
yuki1432 <42318210+yuki1432@users.noreply.github.com>
2019-12-06 00:47:24 +0800

    origin

---
+++ Assets/Scripts/EX/AnimationEx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace EX
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    //播放帧动画的类
+    public class AnimationEx: MonoBehaviour
+    {
+        [SerializeField]
+        private Sprite _frame;
+
+        private void Awake()
+        {
+//            this._frame = this.GetComponent<SpriteRenderer>().sprite;
+//            this._frame = Resources.Load<Sprite>("Effect/loading/00013.png");
+            GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Effect/loading/00013");
+        }
+    }
+}
