c1488589c31bfccbf06db1c9995c16945c3bee0c
yuki1432 <42318210+yuki1432@users.noreply.github.com>
2019-12-22 23:34:37 +0800

    origin

---
+++ Assets/Scripts/LuaFramework/LuaModules.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Util;
+
+namespace LuaFramework
+{
+    //自动加载目标模块
+    public class LuaModules
+    {
+        public void LoadModule(string name)
+        {
+            var str = Loader.Load<TextAsset>(name);
+            //使用LuaEngine去加载这个模块中的init文件
+        }
+    }
+}
