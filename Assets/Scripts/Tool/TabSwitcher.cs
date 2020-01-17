800f578f2c665cffae3b99cc1f6806c4903a89cb
yuki1432 <42318210+yuki1432@users.noreply.github.com>
2020-01-18 01:08:50 +0800

    origin

--- Assets/Scripts/Tool/TabSwitcher.cs
+++ Assets/Scripts/Tool/TabSwitcher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using Util;
 using KeyCode = Lib.KeyCode;
 
@@ -16,13 +17,49 @@ namespace Tool
         [Tooltip("要切换的组件列表")]
         public GameObject[] components;
 
+        private int _curIdx;
+
 
         private void Start()
         {
-//            Listener.Instance.On("tab", KeyCode.Tab, null, () =>
-//            {
-//                
-//            });
+            Listener.Instance.On("", KeyCode.Tab, null, () =>
+            {
+                var e = EventSystem.current;
+                var flag = false;
+                if (e.currentSelectedGameObject != null && e.currentSelectedGameObject.GetHashCode() == gameObject.GetHashCode())
+                {
+                    _curIdx = -1;
+                }
+                else
+                {
+                    for (int i = 0; i < components.Length; i++)
+                    {
+                        if (components[i].GetHashCode() == e.currentSelectedGameObject.GetHashCode())
+                        {
+                            _curIdx = i;
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (!flag)
+                    {
+                        _curIdx = -2;
+                    }
+                }
+
+                if (_curIdx == -2)
+                {
+                    return;
+                }
+
+                _curIdx++;
+                if (_curIdx >= components.Length)
+                {
+                    _curIdx = 0;
+                }
+
+                e.SetSelectedGameObject(components[_curIdx]);
+            });
         }
     }
 }
