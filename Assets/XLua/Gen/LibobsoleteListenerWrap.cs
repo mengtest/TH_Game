ae31775a09eb89d0a81047469d8fb8a3baae8e4b
yuki1432 <42318210+yuki1432@users.noreply.github.com>
2020-06-02 19:33:06 +0800

    增加meta文件

---
+++ Assets/XLua/Gen/LibobsoleteListenerWrap.cs
@@ -0,0 +1,297 @@
+﻿#if USE_UNI_LUA
+using LuaAPI = UniLua.Lua;
+using RealStatePtr = UniLua.ILuaState;
+using LuaCSFunction = UniLua.CSharpFunctionDelegate;
+#else
+using LuaAPI = XLua.LuaDLL.Lua;
+using RealStatePtr = System.IntPtr;
+using LuaCSFunction = XLua.LuaDLL.lua_CSFunction;
+#endif
+
+using XLua;
+using System.Collections.Generic;
+
+
+namespace XLua.CSObjectWrap
+{
+    using Utils = XLua.Utils;
+    public class LibobsoleteListenerWrap 
+    {
+        public static void __Register(RealStatePtr L)
+        {
+			ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			System.Type type = typeof(Lib.obsolete.Listener);
+			Utils.BeginObjectRegister(type, L, translator, 0, 4, 0, 0);
+			
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "On", _m_On);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "Off", _m_Off);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "Event", _m_Event);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "OffAll", _m_OffAll);
+			
+			
+			
+			
+			
+			Utils.EndObjectRegister(type, L, translator, null, null,
+			    null, null, null);
+
+		    Utils.BeginClassRegister(type, L, __CreateInstance, 4, 1, 0);
+			Utils.RegisterFunc(L, Utils.CLS_IDX, "Init", _m_Init_xlua_st_);
+            
+			
+            Utils.RegisterObject(L, translator, Utils.CLS_IDX, "KEY_EVENT", Lib.obsolete.Listener.KEY_EVENT);
+            Utils.RegisterObject(L, translator, Utils.CLS_IDX, "MOUSE_EVENT", Lib.obsolete.Listener.MOUSE_EVENT);
+            
+			Utils.RegisterFunc(L, Utils.CLS_GETTER_IDX, "Instance", _g_get_Instance);
+            
+			
+			
+			Utils.EndClassRegister(type, L, translator);
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int __CreateInstance(RealStatePtr L)
+        {
+            
+			try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+				if(LuaAPI.lua_gettop(L) == 1)
+				{
+					
+					Lib.obsolete.Listener gen_ret = new Lib.obsolete.Listener();
+					translator.Push(L, gen_ret);
+                    
+					return 1;
+				}
+				
+			}
+			catch(System.Exception gen_e) {
+				return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+			}
+            return LuaAPI.luaL_error(L, "invalid arguments to Lib.obsolete.Listener constructor!");
+            
+        }
+        
+		
+        
+		
+        
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_Init_xlua_st_(RealStatePtr L)
+        {
+		    try {
+            
+            
+            
+                
+                {
+                    
+                    Lib.obsolete.Listener.Init(  );
+                    
+                    
+                    
+                    return 0;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_On(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                Lib.obsolete.Listener gen_to_be_invoked = (Lib.obsolete.Listener)translator.FastGetCSObj(L, 1);
+            
+            
+			    int gen_param_count = LuaAPI.lua_gettop(L);
+            
+                if(gen_param_count == 6&& (LuaAPI.lua_isnil(L, 2) || LuaAPI.lua_type(L, 2) == LuaTypes.LUA_TSTRING)&& translator.Assignable<Lib.obsolete.Listener.Function>(L, 3)&& LuaTypes.LUA_TNUMBER == LuaAPI.lua_type(L, 4)&& translator.Assignable<object>(L, 5)&& LuaTypes.LUA_TBOOLEAN == LuaAPI.lua_type(L, 6)) 
+                {
+                    string _eventName = LuaAPI.lua_tostring(L, 2);
+                    Lib.obsolete.Listener.Function _func = translator.GetDelegate<Lib.obsolete.Listener.Function>(L, 3);
+                    int _keycode = LuaAPI.xlua_tointeger(L, 4);
+                    object _caller = translator.GetObject(L, 5, typeof(object));
+                    bool _once = LuaAPI.lua_toboolean(L, 6);
+                    
+                    gen_to_be_invoked.On( _eventName, _func, _keycode, _caller, _once );
+                    
+                    
+                    
+                    return 0;
+                }
+                if(gen_param_count == 5&& (LuaAPI.lua_isnil(L, 2) || LuaAPI.lua_type(L, 2) == LuaTypes.LUA_TSTRING)&& translator.Assignable<Lib.obsolete.Listener.Function>(L, 3)&& LuaTypes.LUA_TNUMBER == LuaAPI.lua_type(L, 4)&& translator.Assignable<object>(L, 5)) 
+                {
+                    string _eventName = LuaAPI.lua_tostring(L, 2);
+                    Lib.obsolete.Listener.Function _func = translator.GetDelegate<Lib.obsolete.Listener.Function>(L, 3);
+                    int _keycode = LuaAPI.xlua_tointeger(L, 4);
+                    object _caller = translator.GetObject(L, 5, typeof(object));
+                    
+                    gen_to_be_invoked.On( _eventName, _func, _keycode, _caller );
+                    
+                    
+                    
+                    return 0;
+                }
+                if(gen_param_count == 4&& (LuaAPI.lua_isnil(L, 2) || LuaAPI.lua_type(L, 2) == LuaTypes.LUA_TSTRING)&& translator.Assignable<Lib.obsolete.Listener.Function>(L, 3)&& LuaTypes.LUA_TNUMBER == LuaAPI.lua_type(L, 4)) 
+                {
+                    string _eventName = LuaAPI.lua_tostring(L, 2);
+                    Lib.obsolete.Listener.Function _func = translator.GetDelegate<Lib.obsolete.Listener.Function>(L, 3);
+                    int _keycode = LuaAPI.xlua_tointeger(L, 4);
+                    
+                    gen_to_be_invoked.On( _eventName, _func, _keycode );
+                    
+                    
+                    
+                    return 0;
+                }
+                if(gen_param_count == 3&& (LuaAPI.lua_isnil(L, 2) || LuaAPI.lua_type(L, 2) == LuaTypes.LUA_TSTRING)&& translator.Assignable<Lib.obsolete.Listener.Function>(L, 3)) 
+                {
+                    string _eventName = LuaAPI.lua_tostring(L, 2);
+                    Lib.obsolete.Listener.Function _func = translator.GetDelegate<Lib.obsolete.Listener.Function>(L, 3);
+                    
+                    gen_to_be_invoked.On( _eventName, _func );
+                    
+                    
+                    
+                    return 0;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+            return LuaAPI.luaL_error(L, "invalid arguments to Lib.obsolete.Listener.On!");
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_Off(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                Lib.obsolete.Listener gen_to_be_invoked = (Lib.obsolete.Listener)translator.FastGetCSObj(L, 1);
+            
+            
+			    int gen_param_count = LuaAPI.lua_gettop(L);
+            
+                if(gen_param_count == 2&& translator.Assignable<object>(L, 2)) 
+                {
+                    object _caller = translator.GetObject(L, 2, typeof(object));
+                    
+                    gen_to_be_invoked.Off( _caller );
+                    
+                    
+                    
+                    return 0;
+                }
+                if(gen_param_count == 2&& (LuaAPI.lua_isnil(L, 2) || LuaAPI.lua_type(L, 2) == LuaTypes.LUA_TSTRING)) 
+                {
+                    string _caller = LuaAPI.lua_tostring(L, 2);
+                    
+                    gen_to_be_invoked.Off( _caller );
+                    
+                    
+                    
+                    return 0;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+            return LuaAPI.luaL_error(L, "invalid arguments to Lib.obsolete.Listener.Off!");
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_Event(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                Lib.obsolete.Listener gen_to_be_invoked = (Lib.obsolete.Listener)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    string _eventName = LuaAPI.lua_tostring(L, 2);
+                    object[] _objs = translator.GetParams<object>(L, 3);
+                    
+                    gen_to_be_invoked.Event( _eventName, _objs );
+                    
+                    
+                    
+                    return 0;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_OffAll(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                Lib.obsolete.Listener gen_to_be_invoked = (Lib.obsolete.Listener)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    
+                    gen_to_be_invoked.OffAll(  );
+                    
+                    
+                    
+                    return 0;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _g_get_Instance(RealStatePtr L)
+        {
+		    try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			    translator.Push(L, Lib.obsolete.Listener.Instance);
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            return 1;
+        }
+        
+        
+        
+		
+		
+		
+		
+    }
+}
---
+++ Assets/XLua/Gen/PrefabCardDisplayScript2Wrap.cs
@@ -0,0 +1,296 @@
+﻿#if USE_UNI_LUA
+using LuaAPI = UniLua.Lua;
+using RealStatePtr = UniLua.ILuaState;
+using LuaCSFunction = UniLua.CSharpFunctionDelegate;
+#else
+using LuaAPI = XLua.LuaDLL.Lua;
+using RealStatePtr = System.IntPtr;
+using LuaCSFunction = XLua.LuaDLL.lua_CSFunction;
+#endif
+
+using XLua;
+using System.Collections.Generic;
+
+
+namespace XLua.CSObjectWrap
+{
+    using Utils = XLua.Utils;
+    public class PrefabCardDisplayScript2Wrap 
+    {
+        public static void __Register(RealStatePtr L)
+        {
+			ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			System.Type type = typeof(Prefab.CardDisplayScript2);
+			Utils.BeginObjectRegister(type, L, translator, 0, 3, 4, 4);
+			
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "OnPointerClick", _m_OnPointerClick);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "OnPointerDown", _m_OnPointerDown);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "OnPointerUp", _m_OnPointerUp);
+			
+			
+			Utils.RegisterFunc(L, Utils.GETTER_IDX, "data", _g_get_data);
+            Utils.RegisterFunc(L, Utils.GETTER_IDX, "content", _g_get_content);
+            Utils.RegisterFunc(L, Utils.GETTER_IDX, "boarder", _g_get_boarder);
+            Utils.RegisterFunc(L, Utils.GETTER_IDX, "count", _g_get_count);
+            
+			Utils.RegisterFunc(L, Utils.SETTER_IDX, "data", _s_set_data);
+            Utils.RegisterFunc(L, Utils.SETTER_IDX, "content", _s_set_content);
+            Utils.RegisterFunc(L, Utils.SETTER_IDX, "boarder", _s_set_boarder);
+            Utils.RegisterFunc(L, Utils.SETTER_IDX, "count", _s_set_count);
+            
+			
+			Utils.EndObjectRegister(type, L, translator, null, null,
+			    null, null, null);
+
+		    Utils.BeginClassRegister(type, L, __CreateInstance, 2, 0, 0);
+			
+			
+            Utils.RegisterObject(L, translator, Utils.CLS_IDX, "ClickEvent", Prefab.CardDisplayScript2.ClickEvent);
+            
+			
+			
+			
+			Utils.EndClassRegister(type, L, translator);
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int __CreateInstance(RealStatePtr L)
+        {
+            
+			try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+				if(LuaAPI.lua_gettop(L) == 1)
+				{
+					
+					Prefab.CardDisplayScript2 gen_ret = new Prefab.CardDisplayScript2();
+					translator.Push(L, gen_ret);
+                    
+					return 1;
+				}
+				
+			}
+			catch(System.Exception gen_e) {
+				return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+			}
+            return LuaAPI.luaL_error(L, "invalid arguments to Prefab.CardDisplayScript2 constructor!");
+            
+        }
+        
+		
+        
+		
+        
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_OnPointerClick(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                Prefab.CardDisplayScript2 gen_to_be_invoked = (Prefab.CardDisplayScript2)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    UnityEngine.EventSystems.PointerEventData _eventData = (UnityEngine.EventSystems.PointerEventData)translator.GetObject(L, 2, typeof(UnityEngine.EventSystems.PointerEventData));
+                    
+                    gen_to_be_invoked.OnPointerClick( _eventData );
+                    
+                    
+                    
+                    return 0;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_OnPointerDown(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                Prefab.CardDisplayScript2 gen_to_be_invoked = (Prefab.CardDisplayScript2)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    UnityEngine.EventSystems.PointerEventData _eventData = (UnityEngine.EventSystems.PointerEventData)translator.GetObject(L, 2, typeof(UnityEngine.EventSystems.PointerEventData));
+                    
+                    gen_to_be_invoked.OnPointerDown( _eventData );
+                    
+                    
+                    
+                    return 0;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_OnPointerUp(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                Prefab.CardDisplayScript2 gen_to_be_invoked = (Prefab.CardDisplayScript2)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    UnityEngine.EventSystems.PointerEventData _eventData = (UnityEngine.EventSystems.PointerEventData)translator.GetObject(L, 2, typeof(UnityEngine.EventSystems.PointerEventData));
+                    
+                    gen_to_be_invoked.OnPointerUp( _eventData );
+                    
+                    
+                    
+                    return 0;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _g_get_data(RealStatePtr L)
+        {
+		    try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			
+                Prefab.CardDisplayScript2 gen_to_be_invoked = (Prefab.CardDisplayScript2)translator.FastGetCSObj(L, 1);
+                translator.PushAny(L, gen_to_be_invoked.data);
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            return 1;
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _g_get_content(RealStatePtr L)
+        {
+		    try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			
+                Prefab.CardDisplayScript2 gen_to_be_invoked = (Prefab.CardDisplayScript2)translator.FastGetCSObj(L, 1);
+                translator.Push(L, gen_to_be_invoked.content);
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            return 1;
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _g_get_boarder(RealStatePtr L)
+        {
+		    try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			
+                Prefab.CardDisplayScript2 gen_to_be_invoked = (Prefab.CardDisplayScript2)translator.FastGetCSObj(L, 1);
+                translator.Push(L, gen_to_be_invoked.boarder);
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            return 1;
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _g_get_count(RealStatePtr L)
+        {
+		    try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			
+                Prefab.CardDisplayScript2 gen_to_be_invoked = (Prefab.CardDisplayScript2)translator.FastGetCSObj(L, 1);
+                translator.Push(L, gen_to_be_invoked.count);
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            return 1;
+        }
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _s_set_data(RealStatePtr L)
+        {
+		    try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			
+                Prefab.CardDisplayScript2 gen_to_be_invoked = (Prefab.CardDisplayScript2)translator.FastGetCSObj(L, 1);
+                gen_to_be_invoked.data = translator.GetObject(L, 2, typeof(object));
+            
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            return 0;
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _s_set_content(RealStatePtr L)
+        {
+		    try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			
+                Prefab.CardDisplayScript2 gen_to_be_invoked = (Prefab.CardDisplayScript2)translator.FastGetCSObj(L, 1);
+                gen_to_be_invoked.content = (UnityEngine.UI.Image)translator.GetObject(L, 2, typeof(UnityEngine.UI.Image));
+            
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            return 0;
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _s_set_boarder(RealStatePtr L)
+        {
+		    try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			
+                Prefab.CardDisplayScript2 gen_to_be_invoked = (Prefab.CardDisplayScript2)translator.FastGetCSObj(L, 1);
+                gen_to_be_invoked.boarder = (UnityEngine.UI.Image)translator.GetObject(L, 2, typeof(UnityEngine.UI.Image));
+            
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            return 0;
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _s_set_count(RealStatePtr L)
+        {
+		    try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			
+                Prefab.CardDisplayScript2 gen_to_be_invoked = (Prefab.CardDisplayScript2)translator.FastGetCSObj(L, 1);
+                gen_to_be_invoked.count = (UnityEngine.UI.Text)translator.GetObject(L, 2, typeof(UnityEngine.UI.Text));
+            
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            return 0;
+        }
+        
+		
+		
+		
+		
+    }
+}
