dd46d590b8c8c06a3acc58c7b11f36f2d0c1f01c
yuki1432 <42318210+yuki1432@users.noreply.github.com>
2020-08-12 20:00:28 +0800

    origin

---
+++ Assets/Scripts/Extend/Util.cs
@@ -0,0 +1,12 @@
+// public class Extend
+// {
+//     public static UnityEngine.GameObject CreateCombatCard()
+//     {
+//         return null;
+//     }
+
+//     public static UnityEngine.GameObject InitCombatCard()
+//     {
+//         return null;
+//     }
+// }
